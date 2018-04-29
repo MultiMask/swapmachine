4438afc0b0f3662adc60cfa540a2e066035f32ab
d.radkovskiy <d.radkovskiy@crazypanda.ru>
2018-04-30 00:45:48 +0300

    karma txid

--- js/karma.js
+++ js/karma.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var karmajs_1 = require("karmajs");
-function sendTransaction(from, pubKey, privateKey, to, amount, callback) {
+var karmajs_ws_1 = require("karmajs-ws");
+function sendTransaction(from, pubKey, privateKey, to, amount, memoText, callback) {
     function toHex(str) {
         var hex = '';
         for (var i = 0; i < str.length; i++) {
@@ -12,11 +13,23 @@ function sendTransaction(from, pubKey, privateKey, to, amount, callback) {
     var tr = new karmajs_1.TransactionBuilder();
     var precision = 5;
     var pkey = karmajs_1.PrivateKey.fromWif(privateKey);
+    karmajs_ws_1.ChainConfig.networks.Karma = {
+        core_asset: 'KRMT',
+        address_prefix: 'KRMT',
+        chain_id: 'e81bea67cebfe8612010fc7c26702bce10dc53f05c57ee6d5b720bbe62e51bef',
+    };
+    karmajs_ws_1.ChainConfig.setPrefix('KRMT');
     var operationParams = {
         fee: { amount: 0, asset_id: '1.3.0' },
         from: from,
         to: to,
         amount: { amount: Math.floor(amount * Math.pow(10, precision)), asset_id: '1.3.0' },
+        memo: {
+            from: pubKey,
+            to: pubKey,
+            nonce: karmajs_1.TransactionHelper.unique_nonce_uint64(),
+            message: toHex(memoText),
+        },
     };
     console.log(operationParams);
     tr.add_type_operation('transfer', operationParams);
--- src/karma.ts
+++ src/karma.ts
@@ -1,6 +1,7 @@
 import { TransactionBuilder, PrivateKey, TransactionHelper } from "karmajs"
+import { ChainConfig } from "karmajs-ws"
 
-export function sendTransaction(from: string, pubKey: string, privateKey: string, to: string, amount: number, callback: (err, res) => void)
+export function sendTransaction(from: string, pubKey: string, privateKey: string, to: string, amount: number, memoText: string, callback: (err, res) => void)
 {
 	function toHex(str)
 	{
@@ -15,17 +16,26 @@ export function sendTransaction(from: string, pubKey: string, privateKey: string
 	const pkey = PrivateKey.fromWif(privateKey)
 	
 	
+	ChainConfig.networks.Karma = {
+		core_asset:  'KRMT',
+		address_prefix:  'KRMT',
+		chain_id: 'e81bea67cebfe8612010fc7c26702bce10dc53f05c57ee6d5b720bbe62e51bef',
+	}
+	
+	
+	ChainConfig.setPrefix('KRMT')
+
 	let operationParams = {
 		fee: {amount: 0, asset_id: '1.3.0'},
 		from,
 		to,
 		amount: {amount: Math.floor(amount * Math.pow(10, precision)), asset_id: '1.3.0'},
-		/* memo: {
-			from: 'KRMT7nKczna7E67Q5JntfeaKfhK3mTnZai6euTzj5tsfebW2W6iEmE',
-			to: 'KRMT6y4SbupANg4iPAQ9YNh7pSkTYTPcZ8e8tuDszZezCFDXiP25ie',
+		memo: {
+			from: pubKey,
+			to: pubKey,
 			nonce: TransactionHelper.unique_nonce_uint64(),
-			message: toHex('testmsg'),
-		}, */
+			message: toHex(memoText),
+		},
 	}
 	
 	
