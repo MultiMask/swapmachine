e4f54545db12c2be6f0ac42a75f866059e53037b
d.radkovskiy <d.radkovskiy@crazypanda.ru>
2018-04-30 00:46:02 +0300

    garbage & increased mining fee

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -13,13 +13,15 @@ function getInfo() {
     var url = 'https://testnet.blockchain.info/rawaddr/';
     return axios_1.default.get("" + url + address).then(function (res) {
         var lastOUT = res.data.txs[0];
-        var outputIndex = lastOUT.out.findIndex(function (item) { return item.addr === address; });
+        // console.log(lastOUT)
+        var outputIndex = lastOUT.out. /* slice().reverse(). */findIndex(function (item) { return item.addr === address; });
         // console.log(outputIndex)
         // console.log(res.data)
         return {
             index: outputIndex,
             address: res.data.address,
             output: lastOUT.hash,
+            // outputs: lastOUT.out.filter(x => x.addr === address),
             balance: res.data.final_balance,
             txs: res.data.txs
         };
@@ -38,15 +40,22 @@ function createTransaction(to, amount, data) {
         // console.log('balance: ', balance)
         var output = _a.output, balance = _a.balance, index = _a.index;
         var SUM = balance;
+        // SUM = outputs.reduce((prev, cur) => prev + cur.value, 0)
+        if ((SUM - 15000) < amount)
+            throw "not enough money to send! has: " + SUM + ", need: " + amount;
+        // console.log(`has: ${SUM}, need: ${amount}\noutput:${output}`)
         var testnet = bitcoin.networks.testnet;
         var bitcoin_payload = Buffer.from(data, 'utf8');
         var dataScript = bitcoin.script.nullData.output.encode(bitcoin_payload);
         var keyPair = bitcoin.ECPair.fromWIF(pk, testnet);
+        // console.log(outputs)
         var txb = new bitcoin.TransactionBuilder(testnet);
+        // outputs.forEach(x => txb.addInput(output, x.n))
+        // console.log(txb)
         txb.addInput(output, index);
         txb.addOutput(dataScript, 0);
         txb.addOutput(to, amount);
-        txb.addOutput(address, SUM - amount - 5000);
+        txb.addOutput(address, SUM - amount - 15000);
         txb.sign(0, keyPair);
         return txb.build().toHex();
     });
--- src/bitcoin.ts
+++ src/bitcoin.ts
@@ -17,7 +17,8 @@ export function getInfo()
 	return axios.get(`${url}${address}`).then(res =>
 	{
 		const lastOUT = res.data.txs[0]
-		const outputIndex = lastOUT.out.findIndex(item => item.addr === address)
+		// console.log(lastOUT)
+		const outputIndex = lastOUT.out./* slice().reverse(). */findIndex(item => item.addr === address)
 
 		// console.log(outputIndex)
 		// console.log(res.data)
@@ -26,6 +27,7 @@ export function getInfo()
 			index: outputIndex,
 			address: res.data.address,
 			output: lastOUT.hash,
+			// outputs: lastOUT.out.filter(x => x.addr === address),
 			balance: res.data.final_balance,
 			txs: res.data.txs
 		}
@@ -33,7 +35,7 @@ export function getInfo()
 }
 export function createTransaction(to, amount: number, data)
 {
-	return getInfo().then(({ output, balance, index }) =>
+	return getInfo().then(({ output, balance, index, /* outputs */ }) =>
 	{
 
 		// SEND signed Tx
@@ -47,17 +49,27 @@ export function createTransaction(to, amount: number, data)
   
 		let SUM = balance
 
+		// SUM = outputs.reduce((prev, cur) => prev + cur.value, 0)
+
+		if ((SUM - 15000) < amount)
+			throw `not enough money to send! has: ${SUM}, need: ${amount}`
+		
+		// console.log(`has: ${SUM}, need: ${amount}\noutput:${output}`)
+
 		let testnet = bitcoin.networks.testnet
 		let bitcoin_payload = Buffer.from(data, 'utf8')
 		let dataScript = bitcoin.script.nullData.output.encode(bitcoin_payload)
 		let keyPair = bitcoin.ECPair.fromWIF(pk, testnet)
 	
+		// console.log(outputs)
 		let txb = new bitcoin.TransactionBuilder(testnet)
+		// outputs.forEach(x => txb.addInput(output, x.n))
+		// console.log(txb)
 		txb.addInput(output, index)
 	
 		txb.addOutput(dataScript, 0)
 		txb.addOutput(to, amount)
-		txb.addOutput(address, SUM - amount - 5000)
+		txb.addOutput(address, SUM - amount - 15000)
 		txb.sign(0, keyPair)
 
 		return txb.build().toHex()
